0fafcfc7d80c0f37f6117cdbfd4d1d8f70515bfd
Oussama Bougaila <oussama.bougaila@gmail.com>
2020-04-26 17:01:04 +0100

    feat: Create templates

--- assignment/urls.py
+++ assignment/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from products.views import (
+    ProductListView,
+    ProductDetailView
+)
 
 urlpatterns = [
+    path('', ProductListView.as_view(), name='list'),
+    path('products/<int:pk>', ProductDetailView.as_view(), name='detail'),
     path('admin/', admin.site.urls),
 ]
