5254b5c115b467fe960a788a5671c1512e0b6dcb
Oussama Bougaila <oussama.bougaila@gmail.com>
2020-04-26 15:28:51 +0100

    feat: Add product list and detail views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+"""Represent Django views"""
 
-# Create your views here.
+from django.views.generic import (
+    ListView,
+    DetailView
+)
+from django.utils import timezone
+
+from .models import Product
+
+
+class ProductListView(ListView):
+    """Represent product list view."""
+    model = Product
+    paginate_by = 6
+
+    def get_queryset(self):
+        """Return the available products."""
+        return Product.objects.filter(publishing_end_date__gt=timezone.now())
+
+
+class ProductDetailView(DetailView):
+    """Represent product detail view."""
+
+    def get_queryset(self):
+        """
+        Excludes any product with expired publishing_end_date.
+        """
+        return Product.objects.filter(publishing_end_date__gt=timezone.now())
